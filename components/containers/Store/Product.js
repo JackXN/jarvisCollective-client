eebf361af2da96865d4cb9c04b5375e3a29276b4
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2022-04-26 18:59:11 -0600

    db connection +++

--- components/containers/Store/Product.js
+++ components/containers/Store/Product.js
@@ -15,7 +15,7 @@ import { useDispatch } from "react-redux";
 
 
 
-function Product() {
+function Product({item}) {
 
     const id = location.pathname.split('/')[2];
     const [product,setProduct] = useState({});
@@ -52,23 +52,7 @@ function Product() {
 
       return (
         <Box sx={styles.container}>
-            <Box sx={styles.cardInfo}>
-            <img src={item.img}/>
-            <Box sx={styles.desc}>
-           <Text as='p' fontSize='35px' fontFamily='Bangers,san-serif' background='red' color='black'fontWeight='bold' textTransform='uppercase'>{item.title}</Text>
-           <Box sx={styles.infoStuff}>
-           <Text as='p'  fontFamily='Bangers,san-serif'  color='white' fontSize='20px' pt='-20px' mr='20px' ml='20px'fontWeight='light' >{item.description}</Text>
-            <Text as='p'><span style={{background: 'blue', color:'white', padding: '10px'}}>Category:</span> <span style={{background: 'white', padding: '10px', color: 'black'}}>{item.category}</span></Text>
-            <p><span style={{background: 'red', padding: '10px' }}>Price:</span><span style={{background: 'white', padding:'10px'}}> {item.price}$</span></p>
-            <p><span style={{background: 'lightgreen', padding: '10px'}}>Collection Name:</span> <span style={{background: 'white', padding: '10px'}}>{item.collectionName}</span></p>
-            <p><span style={{background: 'orange', padding: '10px'}}>In Stock:</span><span style={{
-              background: 'white',
-              padding: '10px'
-            }}>{item.inStock}</span></p>
-            <p><span style={{background:'pink',padding: '10px'}}>Color:</span><span style={{background: 'white', padding: '10px'}}> {item.color}</span></p>
-            </Box>
-            </Box>
-            </Box>
+        
       </Box>
      )
  }
@@ -86,22 +70,7 @@ function Product() {
          
      },
  
-     desc: {
- textAlign: 'center',
- background: '#525353',
- borderRadius: '20px',
- height: '500',
- 
-     },
-     cardInfo: {
-      background: '#2F2E2E',
-      borderRadius: '20px'
-     },
-     infoStuff: {
-       p: {
-         padding: '10px'
-       }
-     }
+
  }
  
  export default Product
--- pages/store.js
+++ pages/store.js
@@ -1,82 +1,91 @@
-import {Link} from 'react-router-dom';
-import Image from '@chakra-ui/image';
+import { Link } from "react-router-dom";
+import Image from "@chakra-ui/image";
 
-import Product from '../components/containers/Store/Product';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import {Box, Text} from '@chakra-ui/react'
+import Product from "../components/containers/Store/Product";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Box, StylesProvider, Text } from "@chakra-ui/react";
 
-const BASE_URL = 'http://localhost:5000'
+const BASE_URL = "http://localhost:5000";
 
-const Products = ({ cat, filters,sort }) => {
-    const [products,setProducts] = useState([]);
-    const [filteredProducts,setFilteredProducts] = useState([]);
-    useEffect(() => {
-        const getProducts = async () => {
-          try {
-            const res = await axios.get(
-              cat
-                ? `${BASE_URL}/api/collections?category=${cat}`
-                : `${BASE_URL}/api/collections`
-            );
-            setProducts(res.data);
-            console.log(res.data)
-          } catch (err) {}
-        };
-        getProducts();
-      }, [cat]);
-    
-      useEffect(() => {
-        cat &&
-          setFilteredProducts(
-            products.filter((item) =>
-              Object.entries(filters).every(([key, value]) =>
-                item[key].includes(value)
-              )
-            )
-          );
-      }, [products, cat, filters]);
-    
-      useEffect(() => {
-        if (sort === "newest") {
-          setFilteredProducts((prev) =>
-            [...prev].sort((a, b) => a.createdAt - b.createdAt)
-          );
-        } else if (sort === "asc") {
-          setFilteredProducts((prev) =>
-            [...prev].sort((a, b) => a.price - b.price)
-          );
-        } else {
-          setFilteredProducts((prev) =>
-            [...prev].sort((a, b) => b.price - a.price)
-          );
-        }
-      }, [sort]);
-    return (
-       <>
-       <Text as='h1' textAlign='center' fontSize='50px' color='#fff'>Products</Text>
-       <Box sx={styles.container}>
+const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `${BASE_URL}/api/collections?category=${cat}`
+            : `${BASE_URL}/api/collections`
+        );
+        setProducts(res.data);
+        console.log(res.data);
+      } catch (err) {}
+    };
+    getProducts();
+  }, [cat]);
 
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, cat, filters]);
 
-{cat
-        ? filteredProducts.map((item) => <Product item={item} key={item.id} />)
-        : products
-            .slice(0, 8)
-            .map((item) => <Product item={item} key={item.id}/>)}
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
+  return (
+    <>
+      <Box sx={styles.container}>
+<Box sx={styles.wrapper}>
+<Box sx={styles.collectionsContainer}>
+<Text as='h1'>Collections</Text>
+{products.map((item) => <Product item={item} key={item.id}/>)}
 </Box>
-</>
-    
-    )
-}
-
+</Box>
+      </Box>
+    </>
+  );
+};
 
 const styles = {
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        alignItems:'center',
-        justifyContent: 'center'
-    }
-}
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: '30px'
+  },
+  wrapper: {
+    height: '150px',
+    width: '100%',
+    background: 'orange',
+    padding:'10px',
+    fontFamily: 'Anton,san-serif'
+  },
+  collectionsContainer: {
+      textAlign: 'left',
+      fontSize: ['40px']
+      
+  }
+};
 
 export default Products;
